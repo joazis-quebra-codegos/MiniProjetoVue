4f705ad3871993baa980ae580428c063a602d6f8
DAVI MARTINS CORDEIRO <davi_m_cordeiro@estudante.sesisenai.org.br>
2026-04-13 19:45:53 -0300

    a

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
-  </p>
+  </p>djbfksjbdf
 </template>
 
 <style scoped></style>
