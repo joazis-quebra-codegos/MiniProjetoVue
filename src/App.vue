4ac9468916ac501fa2276604cccfd8abed3a6b70
joazis-quebra-codegos <joao.ds.zantti@gmail.com>
2026-04-13 20:00:40 -0300

    a

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,22 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Cargo</th>
+        <th>Salario</th>
+        <th>Ações</th>
+      </tr>
+      <tbody>
+        <tr></tr>
+      </tbody>
+
+    </thead>
+  </table>
 </template>
 
 <style scoped></style>
