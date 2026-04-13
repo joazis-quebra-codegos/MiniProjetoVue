7bf2c6e7c41a2fe3660c83876dff5cfc9db24543
DAVI MARTINS CORDEIRO <davi_m_cordeiro@estudante.sesisenai.org.br>
2026-04-13 20:10:45 -0300

    f

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
 
 <template>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-<<<<<<< HEAD
+  <div class="cadastro" style="display: flex; justify-self: center;width:50%; flex-direction: column; margin-top: 20px;">
+    <label class="form-label">Nome</label>
+ <input class="form-control" type="text" placeholder="Nome" aria-label="Nome">
+  <label class="form-label">Email</label>
+ <input class="form-control" type="email" placeholder="Email" aria-label="Email">
+  <label  class="form-label">Salario</label>
+ <input class="form-control" type="email" placeholder="Salário" aria-label="salario">
+  </div>
+
+
+  
 
   <table>
     <thead>
@@ -24,9 +29,14 @@
 
     </thead>
   </table>
-=======
-  <input class="form-control" type="text" placeholder="Default input" aria-label="default input example">
->>>>>>> 902c5706c3cb132a3661331f7d79b0385db7fd39
 </template>
 
-<style scoped></style>
+<style scoped>
+  body{
+    display: flex;
+    justify-content: center;
+  }
+  .cadastro{
+    width: 50%;
+  }
+</style>
